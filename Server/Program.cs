263f2f6c78ba112ec346bf8123612641dabe61e8
vothedien <theneid28@gmail.com>
2025-08-09 12:10:11 +0700

    test thử Program.cs

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,15 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System;
+using System.Net.Sockets;
 using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Forms;
+using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows.Forms;
 
-namespace Server
+
+
+namespace Client
 {
-    internal class Program
+    public partial class Form1 : Form
     {
-        static void Main(string[] args)
+        TcpClient client;
+        NetworkStream stream;
+        int scoreP1 = 0, scoreP2 = 0;
+
+        public Form1()
         {
+            InitializeComponent();
+            this.Load += Form1_Load;
+        }
+
+        private void btnRock_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                client = new TcpClient("127.0.0.1", 5000);
+                stream = client.GetStream();
+                MessageBox.Show("Connected to Server!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Connection failed: " + ex.Message);
+            }
         }
     }
 }
+
